2a55a64b770e6a90425e434928233c930c6e4785
kevinnaserwan <kevinnaserwan@gmail.com>
2024-07-22 17:35:24 +0700

    Feat: add search page

--- lib/search/search.dart
+++ lib/search/search.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 class Search extends StatefulWidget {
   const Search({super.key});
@@ -11,7 +13,88 @@ class _SearchState extends State<Search> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.white,
+      body: Container(
+        margin: EdgeInsets.only(top: 80),
+        child: Padding(
+          padding: EdgeInsets.symmetric(horizontal: 30),
+          child: Column(
+            children: [
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  GestureDetector(
+                    onTap: () {
+                      Navigator.pop(context);
+                    },
+                    child: SvgPicture.asset(
+                      "assets/Icon/back.svg",
+                      width: 10,
+                      height: 18,
+                    ),
+                  ),
+                  Text(
+                    'Search',
+                    style: GoogleFonts.poppins(
+                      fontSize: 16,
+                      color: const Color(0xFF1A374D),
+                      fontWeight: FontWeight.w600,
+                    ),
+                  ),
+                  GestureDetector(
+                    onTap: () {
+                      Navigator.pop(context);
+                    },
+                    child: SvgPicture.asset(
+                      "assets/Icon/Buy.svg",
+                      width: 20,
+                      height: 20,
+                    ),
+                  ),
+                ],
+              ),
+              SizedBox(height: 30),
+              Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 0.0),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Container(
+                      width: 270,
+                      child: TextField(
+                        decoration: InputDecoration(
+                          contentPadding:
+                              EdgeInsets.only(left: 30, bottom: 14, top: 14),
+                          hintText: "Search...",
+                          border: OutlineInputBorder(
+                            borderRadius: BorderRadius.circular(10.0),
+                            borderSide: BorderSide.none,
+                          ),
+                          filled: true,
+                          fillColor: Colors.grey[200],
+                        ),
+                      ),
+                    ),
+                    Container(
+                      decoration: BoxDecoration(
+                        color: const Color(0xFF406882),
+                        borderRadius: BorderRadius.circular(10),
+                      ),
+                      child: Padding(
+                        padding: const EdgeInsets.all(18.0),
+                        child: Image.asset(
+                          "assets/Icon/filter.png",
+                          width: 16,
+                          height: 16,
+                        ),
+                      ),
+                    )
+                  ],
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
     );
   }
 }
